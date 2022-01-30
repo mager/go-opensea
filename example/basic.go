19303b89b7cc22f247b1b3ddab2c6f60aa5403d8
Mager <andrew.mager@gmail.com>
2022-01-30 12:57:24 -0600

    GetCollection

--- example/basic.go
+++ example/basic.go
@@ -8,15 +8,22 @@ func main() {
 	// Create a new client
 	client := opensea.NewOpenSeaClient("") // Your API key goes here
 
-	// Get all assets
+	// GetAssets
 	address := "0x3b417FaeE9d2ff636701100891DC2755b5321Cc3" // Jay-Z's wallet address
 	assets, err := client.GetAssets(address)
 	if err != nil {
 		// TODO: Handle error
 	}
 
-	// Print the assets
 	for _, asset := range assets {
 		client.Log.Info(asset.Name)
 	}
+
+	// GetCollection
+	collection, err := client.GetCollection("boredapeyachtclub")
+	if err != nil {
+		// TODO: Handle error
+	}
+
+	client.Log.Info(collection.Name)
 }
